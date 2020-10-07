d4a634804dda500e946978d288d8ad44146c5db6
zovia42 <zoviavia@gmail.com>
2020-10-07 19:17:35 +0700

    another little change on header.php

--- static/header.php
+++ static/header.php
@@ -6,7 +6,7 @@
             <div></div> <!-- Jangan dihapus -->
             <ul>
                 <li class="float-right pc"><a href="#">customer care</a></li>
-                <li class="float-right pc"><a href="#">tentang puma store</a></li>
+                <li class="float-right pc"><a href="#">tentang soft fast</a></li>
                 <!-- <li class="float-right mobile"><a href="#">signup</a></li>
                 <li class="float-right mobile"><a href="#">Login</a></li> -->
                 <li class="float-right mobile"><a href="wishlist.php">Wishlist</a></li>
