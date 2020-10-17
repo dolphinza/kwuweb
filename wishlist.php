56b1b79b69255255a74cea4f505fe7b0cdfbce11
Kyuza <47440331+kyuzaa@users.noreply.github.com>
2020-10-17 18:17:15 +0700

    Add wishlist

--- wishlist.php
+++ wishlist.php
@@ -1,3 +1,30 @@
+<?php
+    include "config.php";
+    session_start();
+    if (!isset($_SESSION["email"])) {
+        echo "<script>alert('Login terlebih dahulu !');window.location.href='index.php';</script>";
+    }
+    $email = $_SESSION['email'];
+    $msg = "";
+    if(isset($_GET["id"])) {
+        $id = $_GET["id"];
+        $q = $con->query("SELECT * FROM barang WHERE id_barang = '$id'");
+        if ($q->num_rows > 0) {
+            $email = $_SESSION['email'];
+            $q2 = $con->query("INSERT INTO wishlist VALUES ('$email','$id')");
+            if ($q2) {
+                $msg .= "Berhasil menambah barang ke wishlist";
+                echo "<script>alert('$msg');window.location.href='index.php';</script>";
+            } else {
+                $msg .= "Barang sudah ada di trolly";
+                echo "<script>alert('$msg');window.location.href='index.php';</script>";
+            }
+        } else {
+            $msg .= "Barang dengan id " . strval($_GET["id"]) . " tidak ada";
+            echo "<script>alert('$msg');window.location.href='index.php';</script>";
+        }
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -51,33 +78,43 @@
     <?php include "static/header.php"; ?> 
     <div class="container mt-3">
         <div class="jumbotron pt-3">
-			<?php for($i = 0; $i < 3; $i++){ ?>
-            <div class="card mt-3">
-                <!-- href disesuaikan linknya dengan card -->
-                <a href="viewProduct.php" class="link" style="text-decoration: none;">
-                    <div class="row">
-                        <div class="card-header">
-                            <img src="./img/sepatu.jpg" class="img img-fluid" alt="card">
+			<?php
+                $q = $con->query("SELECT * FROM wishlist WHERE email='$email'");
+                if($q->num_rows > 0) {
+                    while($data = $q->fetch_array()) {
+                        $id = strval($data['id_barang']);
+                        $q2 = $con->query("SELECT * FROM barang WHERE id_barang='$id'");
+                        $data2 = $q2->fetch_array();
+                        $img = "img/".$data2["path"];
+                        $nama_barang = $data2["nama_barang"];
+                        $harga = $data2["harga"];
+            ?>
+                <div class="card mt-3">
+                    <!-- href disesuaikan linknya dengan card -->
+                    <a href="<?php echo 'viewProduct.php?id='. strval($id)?>" class="link" style="text-decoration: none;">
+                        <div class="row">
+                            <div class="card-header">
+                                <img src="<?php echo $img; ?>" class="img img-fluid" alt="card">
+                            </div>
+                            <div class="card-body">
+                                <h5><?php echo $nama_barang; ?></h5>
+                                <p class="harga"><?php echo $harga; ?></p>
+                                <p class="bintang">✨✨✨✨✨ (10)</p>
+                            </div>
                         </div>
-                        <div class="card-body">
-                            <h5>SHOES</h5>
-                            <p class="harga">Rp.500.000</p>
-                            <p class="bintang">✨✨✨✨✨ (10)</p>
-                        </div>
-                    </div>
-                </a>
-                <div class="card-footer">
-                    <div class="row">
-                        <div class="hapus pr-3">
-                            <button class="btn btn-outline-danger">Hapus</button>
-                        </div>
-                        <div class="keranjang">
-                            <button class="btn btn-outline-primary">Tambah Ke Keranjang</button>
+                    </a>
+                    <div class="card-footer">
+                        <div class="row">
+                            <div class="hapus pr-3">
+                                <button class="btn btn-outline-danger" onclick="return manggil('<?php echo $id; ?>','hapus');">Hapus</button>
+                            </div>
+                            <div class="keranjang">
+                                <button class="btn btn-outline-primary" onclick="return manggil('<?php echo $id; ?>','tambah');">Tambah Ke Keranjang</button>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-			<?php } ?>
+			<?php }} ?>
 			
         </div>
     </div>
@@ -87,5 +124,26 @@
     <script src="bootstrap/js/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" ></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" ></script>
+    <script type="text/javascript">
+        function manggil(id,tipe) {
+            $.ajax({
+                type: "POST",
+                url: "function.php",
+                dataType: "JSON",
+                data: {
+                    type: tipe,
+                    id: id
+                },
+                success: function(e) {
+                    alert(e.result);
+                    window.location.href='index.php';
+                },
+                error: function(e) {
+                    console.log(e);
+                }
+            })
+        }
+
+    </script>
 </body>
 </html>
