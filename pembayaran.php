77bf81471512cb92c379cccd515483fe2121402b
kyuzaa <kevinfebrian12@gmail.com>
2021-01-13 22:07:45 +0700

    Update Terbaru

--- pembayaran.php
+++ pembayaran.php
@@ -160,6 +160,7 @@ function toRupiah($angka){
     <script src="https://cdn.jsdelivr.net/npm/es6-promise@4/dist/es6-promise.auto.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    <!-- status 0 = barang diproses, status 1 = barang dikirim, status 2 = barang sudah sampa -->
     <script>
         $("button#bayar").click(function(){
             var alamat = $("textarea#alamat").val();
